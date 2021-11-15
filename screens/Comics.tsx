e04a26bc9211d3fa67681ffd8d5d927769578191
dja1716 <hdum@uwaterloo.ca>
2021-11-15 12:48:58 +0900

    image load on card

--- screens/Comics.tsx
+++ screens/Comics.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   useQuery,
   QueryClient,
@@ -7,16 +7,21 @@ import {
   useInfiniteQuery,
 } from "react-query";
 import { ComicsResponse, ComicsApi } from "../api";
-import { Animated, PanResponder, View } from "react-native";
+import { Animated, PanResponder, View, Image } from "react-native";
+import ComicsCard from "../components/ComicsCard";
 
 interface Props {}
 
 const backgroundImage = require("../sources/AvengersA.png");
 
 export default function Comics(props: Props) {
+  const [comicsCard, setCommicsCard] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [ready, setReady] = useState(false);
+  let preFetchTasks = [];
   // API
   const queryClient = useQueryClient();
-  const { data: comicsData } = useQuery<ComicsResponse>(
+  const { data: comicsData, isLoading } = useQuery<ComicsResponse>(
     ["comics", "allComics"],
     ComicsApi.Comics
   );
@@ -27,7 +32,12 @@ export default function Comics(props: Props) {
   const rotation = position.interpolate({
     inputRange: [-250, 250],
     outputRange: ["-15deg", "15deg"],
-    extrapolate: "extend",
+    extrapolate: "clamp",
+  });
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.7, 1],
+    extrapolate: "clamp",
   });
   // animations
   const onPressIn = Animated.spring(scale, {
@@ -42,6 +52,28 @@ export default function Comics(props: Props) {
     toValue: 0,
     useNativeDriver: true,
   });
+
+  const onDismiss = () => {
+    setIndex((prev) => prev + 1);
+    scale.setValue(1);
+    position.setValue(0);
+    // set index for data
+  };
+
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    tension: 5,
+    useNativeDriver: true,
+    restSpeedThreshold: 200,
+    restDisplacementThreshold: 200,
+  });
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    tension: 5,
+    useNativeDriver: true,
+    restSpeedThreshold: 200,
+    restDisplacementThreshold: 200,
+  });
   // pan responders
   const panResponder = useRef(
     PanResponder.create({
@@ -49,8 +81,14 @@ export default function Comics(props: Props) {
       onPanResponderGrant: () => {
         onPressIn.start();
       },
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goCenter]).start();
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -150) {
+          goLeft.start(onDismiss);
+        } else if (dx > 150) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
       onPanResponderMove: (_, { dx }) => {
         position.setValue(dx);
@@ -60,12 +98,37 @@ export default function Comics(props: Props) {
 
   useEffect(() => {
     if (comicsData) {
-      console.log(comicsData);
+      //console.log(comicsData);
+      setCommicsCard(comicsData.data.results);
+      console.log(comicsData.data.results[0].thumbnail.path);
+      for (let i = 0; i < comicsData.data.results.length; i++) {
+        preFetchTasks.push(
+          Image.prefetch(
+            comicsData.data.results[i].thumbnail.path +
+              "." +
+              comicsData.data.results[i].thumbnail.extension
+          )
+        );
+      }
+      Promise.all(preFetchTasks).then((results) => setReady(true));
     }
   }, [comicsData]);
+  if (!ready || isLoading) return <View></View>;
   return (
     <Container>
       <CardContainer>
+        <AnimatedCard style={{ transform: [{ scale: secondScale }] }}>
+          {comicsCard[index + 1] ? (
+            <CardImage
+              source={{
+                uri:
+                  comicsCard[index + 1].thumbnail.path +
+                  "." +
+                  comicsCard[index + 1].thumbnail.extension,
+              }}
+            />
+          ) : null}
+        </AnimatedCard>
         <AnimatedCard
           {...panResponder.panHandlers}
           style={{
@@ -75,12 +138,28 @@ export default function Comics(props: Props) {
               { translateX: position },
             ],
           }}
-        ></AnimatedCard>
+        >
+          {comicsCard[index] ? (
+            <CardImage
+              source={{
+                uri:
+                  comicsCard[index].thumbnail.path +
+                  "." +
+                  comicsCard[index].thumbnail.extension,
+              }}
+            />
+          ) : null}
+        </AnimatedCard>
         <Background source={backgroundImage} />
       </CardContainer>
     </Container>
   );
 }
+
+const CardImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
 
 const Container = styled.View`
   flex: 1;
